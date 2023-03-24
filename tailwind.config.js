f0b0e46d51c0c71991adc2a5f77f7c401646920d
Noor Mohammad <noor.jsdivs@gmail.com>
2023-03-24 17:10:14 +0400

    added for the first time

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        bodyColor: "#0A192F",
+        textGreen: "#64ffda",
+        textLight: "#ccd6f6",
+        textDark: "#8892b0",
+      },
+      fontFamily: {
+        bodyFont: ["Montserrat", "sans-serif"],
+        titleFont: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
